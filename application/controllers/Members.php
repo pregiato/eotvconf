887327a45c23f0ac227f6a14a4de924081bafdda
Borbs <Borbs@users.noreply.github.com>
2016-02-27 19:55:10 +0100

    Update Members.php
    set_members() tartalmának beírása

--- application/controllers/Members.php
+++ application/controllers/Members.php
@@ -67,7 +67,25 @@ class Members extends CI_Controller {
 		    }
 		    else
 		    {
-		        $this->members_model->set_members();
+		        $this->load->helper('url');
+
+			    $slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+			    $data = array(
+			    	'slug' => $slug,
+			        'name' => $this->input->post('name'),
+			        'email' => $this->input->post('email'),
+			        'university' => $this->input->post('uni'),
+			       	'phone' => $this->input->post('phone'),
+			        'accomodation' => $this->input->post('acc_ans'),
+			        'work_title' => $this->input->post('work'),
+			        'publication' => $this->input->post('pub_ans'),
+			        'summary' => $this->input->post('Text1'),
+			        'leader_name' => $this->input->post('tem_name'),
+			        'leader_email' => $this->input->post('tem_email')
+		    );
+
+		    return $this->db->insert('members', $data);
 		        $this->load->view('members/success');
 		    }
 		}
